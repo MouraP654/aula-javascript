af3bce20d42f5e6a7cc879ecf997588481fabdf0
MouraP654 <rafaelfelipe.peixoto@gmail.com>
2020-12-04 13:04:21 -0300

    Trabalhando com condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+/*var data = new Date();
+alert(data.getDay())
+alert(data.getMonth())
+alert(data.getFullYear())*/
+
+
+
+/*var cont;
+
+for(cont = 0; cont < 5; cont++) {
+    alert (cont)
+}*/
+
+
+/*var cont = 0;
+
+while(cont <= 5) {
+    console.log(cont)
+    cont++
+}*/
+
+/*var idade = prompt("Qual sua idade?");
+if (idade < 18) {
+    alert("Menor de idade")
+}else {
+    alert ("Maior de idade")
+}*/
+
+
 //var nome = "Rafael Moura";
 //var n1 = 25;
 //var n2 = 10;
@@ -20,8 +49,10 @@
 //console.log(list.toString());
 //console.log(list.join(" | "))
 
-var fruta = {nome: "Maçã", cor: "Vermelha", preço: 3.59};
+/*var fruta = {nome: "Maçã", cor: "Vermelha", preço: 3.59};
 console.log(fruta);
 
 var carros = [{marca: "Fiat", modelo: "Argo"}, {marca: "Fiat", modelo: "Siena"}, {marca: "Fiat", modelo: "Estrada"}]
-console.log(carros[1].modelo)
+console.log(carros[1].modelo)*/
+
+
