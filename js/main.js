b18dea585619cd10bd8e29e3eaadcb159fb0a87d
MouraP654 <rafaelfelipe.peixoto@gmail.com>
2020-12-04 12:25:01 -0300

    Inserido Arrays e Objetos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-var nome = "Rafael Moura";
-var n1 = 25;
-var n2 = 10;
-var frase = "Japão é o melhor time do mundo!"
+//var nome = "Rafael Moura";
+//var n1 = 25;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo!"
 //alert(nome + " tem " + "25 anos")
 //alert(idade + idade2)
-console.log (nome);
-console.log(n1 + n2);
-console.log (frase.toUpperCase());
-console.log (frase.toLowerCase());
+//console.log (nome);
+//console.log(n1 + n2);
+//console.log (frase.toUpperCase());
+//console.log (frase.toLowerCase());
 //alert(frase.replace("Japão", "Brasil"))
+
+//trabalhando com arrays
+
+//var list = ["Maçã", "Pera", "Laranja"];
+//list.push("uva");
+//list.pop(1);
+//console.log(list[1]);
+//console.log(list.length);
+//console.log(list.toString());
+//console.log(list.join(" | "))
+
+var fruta = {nome: "Maçã", cor: "Vermelha", preço: 3.59};
+console.log(fruta);
+
+var carros = [{marca: "Fiat", modelo: "Argo"}, {marca: "Fiat", modelo: "Siena"}, {marca: "Fiat", modelo: "Estrada"}]
+console.log(carros[1].modelo)
