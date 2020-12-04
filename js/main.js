2a467d288b422be05eece7b466d1231ee270afb9
MouraP654 <rafaelfelipe.peixoto@gmail.com>
2020-12-04 12:03:38 -0300

    Inserindo pasta de JavaScript no projeto

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Rafael Moura";
+var n1 = 25;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+//alert(nome + " tem " + "25 anos")
+//alert(idade + idade2)
+console.log (nome);
+console.log(n1 + n2);
+console.log (frase.toUpperCase());
+console.log (frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"))
